4066c2805838f6c3e71fd762b4b3b9e33f5bc9d5
dimetrix555 <dimetrix55@gmail.com>
2020-04-27 22:27:41 +0300

    Тип возвращаемого значения метода

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,12 +3,15 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "John";
         person1.age = 50;
-        person1.sayHello();
+
         Person person2 = new Person();
         person2.name = "William";
         person2.age = 35;
-        person2.speak();
-        person2.sayHello();
+
+        int year1 = person1.calculateYearsToRetirement();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии " + year1 + " лет.");
+        System.out.println("Первому человеку до пенсии " + year2 + " лет.");
     }
 }
 
@@ -19,6 +22,11 @@ class Person {
 
     String name;
     int age;
+
+    int calculateYearsToRetirement() {
+        int years = 65-age;
+        return years;
+    }
 
     void speak() {
         for (int i = 0; i<3; i++) {
