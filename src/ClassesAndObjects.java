ade1f8b78ad9606eab71a3748cd4a4342ab21f19
dimetrix555 <dimetrix55@gmail.com>
2020-05-16 02:38:41 +0300

    Урок 18: Инкапсуляция. Сеттеры и геттеры.

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,17 +1,11 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "John";
-        person1.age = 50;
+        person1.setName("");
+        person1.setAge(-4);
+        System.out.println("Выводим значение в main методе: " + person1.getName());
+        System.out.println("Выводим значение в main методе " + person1.getAge());
 
-        Person person2 = new Person();
-        person2.name = "William";
-        person2.age = 35;
-
-        int year1 = person1.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии " + year1 + " лет.");
-        System.out.println("Первому человеку до пенсии " + year2 + " лет.");
     }
 }
 
@@ -20,8 +14,32 @@ class Person {
     // 1. Данные (поля)
     // 2. Действия которые он может совершать (методы)
 
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public void setName(String userName) {
+        if(userName.isEmpty()) {
+            System.out.println("Вы не ввели имя");
+        } else {
+            name = userName;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int userAge) {
+        if(userAge < 0) {
+            System.out.println("Возраст должен быть положительным");
+        } else {
+            age = userAge;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
 
     int calculateYearsToRetirement() {
         int years = 65-age;
